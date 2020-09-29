e8fce63466c6882aa72554adc96fab9402c5b085
user1 <user1@users-MacBook.local>
2020-09-29 21:31:52 +0300

    Updates

---
+++ precache-manifest.90fab7ad11b2d5263fa747bd7d610a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38de7f19d7f984a553e9d01d108f9549",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9c56c31febb2afc57313",
+    "url": "/goit-react-hw-01-components/static/css/main.f1ac0589.chunk.css"
+  },
+  {
+    "revision": "797bb21596cdc20824be",
+    "url": "/goit-react-hw-01-components/static/js/2.46984f79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.46984f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c56c31febb2afc57313",
+    "url": "/goit-react-hw-01-components/static/js/main.c7a1b85a.chunk.js"
+  },
+  {
+    "revision": "a8cf8cd796cb6354ac79",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.8bc159eb.js"
+  }
+]);
